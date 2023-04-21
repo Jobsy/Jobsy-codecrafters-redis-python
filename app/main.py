73197474c85037e4ec87539c2a0a8b20982063e1
Oluwajoba Bello <oluwajobabello@gmail.com>
2023-04-21 23:03:23 +0100

    Implement the ECHO command

--- app/main.py
+++ app/main.py
@@ -1,15 +1,23 @@
 # Uncomment this to pass the first stage
 import threading
 import socket
+from .resp_decoder import RESPDecoder
+
 
 
 def handle_connection(client_connection):
    while True:
-       try:
-           client_connection.recv(1024)  # wait for client to send data
-           client_connection.send(b"+PONG\r\n")
-       except ConnectionError:
-           break  # Stop serving if the client connection is closed
+        try:
+            command, *args = RESPDecoder(client_connection).decode()
+
+            if command == b"ping":
+                client_connection.send(b"+PONG\r\n")
+            elif command == b"echo":
+                client_connection.send(b"$%d\r\n%b\r\n" % (len(args[0]), args[0]))
+            else:
+                client_connection.send(b"-ERR unknown command\r\n")
+        except ConnectionError:
+             break  # Stop serving if the client connection is closed
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
